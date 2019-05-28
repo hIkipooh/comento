4915ce9117908c7b43a953eb2fba75930bd15e0e
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-05-29 02:21:39 +0900

    Adding components to App

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import AdModal from "./components/AdModal.vue";
+import Filter from "./components/Filter.vue";
+import PostModal from "./components/PostModal.vue";
+import Sort from "./components/Sort.vue";
 
 export default {
   name: "app",
