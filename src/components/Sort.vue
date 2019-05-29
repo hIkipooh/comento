17066e93ced3b3cf34346e25686e7b548ef03210
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-05-29 13:42:29 +0900

    linked vue with bootstrap-vue

---
+++ src/components/Sort.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <b-button pill variant="outline-secondary">오름차순</b-button>
+      <b-button pill variant="outline-secondary">내림차순</b-button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
